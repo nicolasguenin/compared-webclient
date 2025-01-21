0baf8a81da4a589d471af58f32bba2e0c67e1b82
nicolasguenin <nicolasguenin@protonmail.com>
2025-01-21 09:41:05 +0100

    feat: add alias configuration to vitest

--- libs/features/vitest.config.ts
+++ libs/features/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -9,5 +10,11 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     globals: true,
+  },
+  resolve: {
+    alias: {
+      '@cpd/ui': path.resolve(__dirname, '../ui/lib/atoms'),
+      '@cpd/shared': path.resolve(__dirname, '../shared/lib'),
+    },
   },
 });
